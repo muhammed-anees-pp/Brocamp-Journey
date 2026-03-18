eedbb1958b12f3f5ad1b97327b1a113dac30ee76
muhammed-anees-pp <muhammedanees.work@gmail.com>
2026-03-18 21:31:10 +0530

    Week 20 some undates done on 18/03/2026

--- Week 20/practicals/src/App.jsx
+++ Week 20/practicals/src/App.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import Practice from './Test'
+import React, { useEffect, useState } from 'react'
 
 function App() {
+  const [quotes, setQuotes] = useState([])
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/quotes")
+    .then(res => res.json())
+    .then((data) => {
+      setQuotes(data.quotes)
+    })
+  },[])
+
+
+
   return (
     <div>
-      <Test />
+      <h2>Quotes</h2>
+      <table border={1} cellPadding={10}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Quote</th>
+            <th>Author</th>
+          </tr>
+        </thead>
+        <tbody>
+          {quotes.map((q) => (
+            <tr key={q.id}>
+              <td>{q.id}</td>
+              <td>{q.quote}</td>
+              <td>{q.author}</td>
+
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+
     </div>
   )
 }
